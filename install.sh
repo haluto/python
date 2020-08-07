13fa2d8f2b611226ebbf6036a714772ebcb01450
haluto <haluto8086@sina.cn>
2020-08-07 16:54:22 +0800

    use soft link for install.

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-# Actually just copy the py file. make it easy to add PATH. just add ./bin/
-cp ./tools/cutFile/cutFile.py ./bin/cutFile
+shell_path=$(pwd)
+bin_path=${shell_path}/bin
+tool_path=${shell_path}/tools
+
+function install_cutFile() {
+  cutFile_tool_path=${tool_path}/cutFile
+  cd ${bin_path}
+  ln -s ${cutFile_tool_path}/cutFile.py cutFile
+  echo "cutFile installed."
+}
+
+install_cutFile
